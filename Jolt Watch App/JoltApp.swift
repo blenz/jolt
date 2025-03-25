97e434376cc1160122fd4ef07ed97b7e94af84ce
blenz <bbluegga@yahoo.com>
2025-03-24 20:20:00 -0700

    initial commit

---
+++ Jolt Watch App/JoltApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoltApp.swift
+//  Jolt Watch App
+//
+//  Created by Brett Lenz on 3/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Jolt_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
